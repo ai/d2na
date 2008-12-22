c7b7b4909c16d3bfe34991e75afc9f5e80d66efc
Andrey A.I. Sitnik <andrey@sitnik.ru>
2008-12-22 06:34:29 +0300

    Loading VM from Evolution

--- evolution/lib/d2na-evolution.rb
+++ evolution/lib/d2na-evolution.rb
@@ -16,3 +16,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
+
+require 'pathname'
+
+begin
+  require Pathname(__FILE__).dirname.expand_path + '../../vm/lib/d2na-vm'
+rescue LoadError
+  puts "Error: Cann't load D2NA Virtual Machine"
+  exit
+end
